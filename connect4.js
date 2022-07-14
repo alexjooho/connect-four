70f0ca4ae1418e38c85e299693db6fbe944603b1
Alex <alexjooho@gmail.com>
2022-07-13 17:56:16 -0700

    check win

--- connect4.js
+++ connect4.js
@@ -150,6 +150,16 @@ function checkForWin() {
 
     // TODO: Check four cells to see if they're all legal & all color of current
     // player
+    for(let cell of cells) {
+      if(board[cell[0]][cell[1]] === null || board[cell[0]][cell[1]] === undefined) {
+        debugger
+        return false;
+      }
+      if(currPlayer !== board[cell[0]][cell[1]]) {
+          return false;
+        }
+    }
+    return true;
 
   }
 
@@ -164,9 +174,9 @@ function checkForWin() {
       // [ [y, x], [y, x], [y, x], [y, x] ]
 
       let horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
-      let vert;
-      let diagDL;
-      let diagDR;
+      let vert = [[y,x], [y+1, x], [y+2, x], [y+3, x]];
+      let diagDL = [[y,x], [y+1, x-1], [y+2, x-2], [y+3, x-3]];
+      let diagDR = [[y,x], [y+1, x+1], [y+2, x+2], [y+3, x+3]];
 
       // find winner (only checking each win-possibility as needed)
       if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
